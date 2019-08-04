322778c5b25f87678701d7a1988d7dcc3c877c4f
Subhash Surana <Subhash.surana23@gmail.com>
2019-08-04 22:34:46 +0800

    creating signin page and sign in components, form-input, custom-butto…

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import ShopPage from './pages/Shop/shop.component'
+import ShopPage from './pages/Shop/shop.component.jsx'
 
 import './App.css';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
 import HomePage from './pages/homepage.component';
+
+// eslint-disable-next-line
+import Header from './components/Header/header.component.jsx' 
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path ='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
