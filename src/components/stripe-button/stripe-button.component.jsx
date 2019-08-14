1902231b70e36c1b5d33c076928d7bef6265f934
Subhash Surana <Subhash.surana23@gmail.com>
2019-08-14 21:57:21 +0800

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_2iG7Tl6zl2QwDgVwzzh1clwq00zyAP67dA";
+
+  const onToken = token => {
+    console.log(token);
+    alert("Payment successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is ${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+          token={onToken}
+          stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
